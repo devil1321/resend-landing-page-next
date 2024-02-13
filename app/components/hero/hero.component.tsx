bc83a8845618a1345b909f4c84c25837615efc18
devil1321 <s.dominik1321@gmail.com>
2024-02-13 21:02:27 +0100

    small fix document

--- app/components/hero/hero.component.tsx
+++ app/components/hero/hero.component.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
+import Details from './details.component'
 import Cube from './cube.component'
-import dynamic from 'next/dynamic'
 const Hero = () => {
-
-  const DetailsNoSSR = dynamic(()=>import('./details.component'),{ ssr: false})
-
   return (
     <div className='md:flex md:justify-between md:items-center'>
-      <DetailsNoSSR />
+      <Details />
       <Cube />
     </div>
   )
