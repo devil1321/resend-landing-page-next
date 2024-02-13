5f7e3279ba4aad745d07e29c6bd28171816d9db7
devil1321 <s.dominik1321@gmail.com>
2024-02-13 20:33:15 +0100

    small fix refs

--- app/components/hero/details.component.tsx
+++ app/components/hero/details.component.tsx
@@ -1,21 +1,25 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { MutableRefObject, useEffect, useRef } from 'react'
 import gsap from 'gsap'
 
 const Details = () => {
 
-  const handleMakeLetters = (node:HTMLHeadingElement | HTMLParagraphElement) =>{
-    const stringArr = node.textContent?.split("") as string[]
-    node.innerHTML = ''
+  const headingRef = useRef() as MutableRefObject<HTMLHeadingElement>
+  const paragraphRef_1 = useRef() as MutableRefObject<HTMLParagraphElement>
+  const paragraphRef_2 = useRef() as MutableRefObject<HTMLParagraphElement>
+
+  const handleMakeLetters = (ref:MutableRefObject<HTMLHeadingElement | HTMLParagraphElement>) =>{
+    const stringArr = ref.current.textContent?.split("") as string[]
+    ref.current.innerHTML = ''
       stringArr.forEach((l:string)=>{
       const span = document.createElement('span')
       span.textContent = l
-      node.appendChild(span)
+      ref.current.appendChild(span)
     })
   }
 
-  const handleAnimate = (node:HTMLHeadingElement | HTMLParagraphElement,delay?:number) =>{
-    const span = node.querySelectorAll('span')
+  const handleAnimate = (ref:MutableRefObject<HTMLHeadingElement | HTMLParagraphElement>,delay?:number) =>{
+    const span = ref.current.querySelectorAll('span')
     const tl = gsap.timeline()
     gsap.to('.hero-details-button-wrapper',{ left:0,opacity:1,duration:1,force3D:true})
     tl.fromTo(span,{ filter:'blur(10px)'},{filter:'blur(0px)',duration:1,delay,stagger:0.1,force3D:true})
@@ -23,12 +27,12 @@ const Details = () => {
   }
 
   useEffect(()=>{
-    handleMakeLetters(document.querySelector('.hero-details-h') as HTMLHeadingElement)
-    handleMakeLetters(document.querySelector('.hero-details-p1') as HTMLParagraphElement)
-    handleMakeLetters(document.querySelector('.hero-details-p2') as HTMLParagraphElement)
-    handleAnimate(document.querySelector('.hero-details-h') as HTMLHeadingElement,1)
-    handleAnimate(document.querySelector('.hero-details-p1') as HTMLParagraphElement,2)
-    handleAnimate(document.querySelector('.hero-details-p2') as HTMLParagraphElement,2)
+    handleMakeLetters(headingRef)
+    handleMakeLetters(paragraphRef_1)
+    handleMakeLetters(paragraphRef_2)
+    handleAnimate(headingRef,1)
+    handleAnimate(paragraphRef_1,2)
+    handleAnimate(paragraphRef_1,2)
   },[])
 
   return (
@@ -36,9 +40,9 @@ const Details = () => {
       <div className='hero-details-button-wrapper my-2 p-[2px] h-fit w-fit rounded-full bg-gradient-to-r from-red-500 to-green-500'>
         <button className="px-2 py-0 bg-gray-900 rounded-full">Introducing Marketing Emails</button>
       </div>
-      <h1 className='hero-details-h text-white text-5xl my-5 font-bold min-w-fit'>Email For Developers</h1>
-      <p className='hero-details-p1 mb-0'>The best way to reach humans instead of spam folders.</p>
-      <p className='hero-details-p2 mb-5'>Deliver transactional and marketing emails at scale.</p>
+      <h1 ref={headingRef} className='hero-details-h text-white text-5xl my-5 font-bold min-w-fit'>Email For Developers</h1>
+      <p ref={paragraphRef_1} className='hero-details-p1 mb-0'>The best way to reach humans instead of spam folders.</p>
+      <p ref={paragraphRef_2} className='hero-details-p2 mb-5'>Deliver transactional and marketing emails at scale.</p>
       <div className="flex">
         <button className="hero-details-button bg-rose-100 hover:bg-rose-200 text-black py-2 px-6 rounded-full font-bold">Get Started</button>
       </div>
